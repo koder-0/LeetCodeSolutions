2b2739cc3cea660bbdb4468cd51d36771f3fa2fe
Kaushal <kaushal.dakshana16@gmail.com>
2022-10-02 17:10:55 +0530

    Time: 55 ms (68.43%), Space: 12.5 MB (72.26%) - LeetHub

---
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        int res = 1;
+        map<pair<int, int>, int> cntMap;
+        int duplicates = 0;
+        int n = points.size();
+        for(int i = 0; i < n; ++i) {
+            cntMap.clear();
+            duplicates = 1;
+            for(int j = i+1; j < n; ++j) {
+                int dx = points[i][0] - points[j][0];
+                int dy = points[i][1] - points[j][1];
+                if(dx == 0 && dy == 0) {
+                    duplicates++;
+                    res = max(res, duplicates);
+                    continue;
+                }
+                int gcd = __gcd(dx, dy);
+                if(dx) dx = dx/gcd;
+                if(dy) dy = dy/gcd;
+                auto iter = cntMap.find({dx,dy});
+                if(iter != cntMap.end()) {
+                    (iter->second)++;
+                    res = max(res, duplicates+(iter->second));
+                }
+                else {
+                    cntMap[{dx,dy}] = 1;
+                    res = max(res, duplicates+1);
+                }
+            }
+        }
+        
+        return res;
+    }
+};
